3e8af02cd956beef027adeb855b7af2ba4eb4d57
Δρ(Garcia-Castellanos) <d.g.c@csic.es>
2025-03-13 15:32:48 +0100

    commit

--- spillover.py
+++ spillover.py
@@ -7,18 +7,18 @@ st.title("Spillover: Water Transfer and Erosion between Lakes")
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    H1_init = st.number_input("Initial Level Lake 1 (m)", -1e9, 1e9, 500, 10)
-    H2_init = st.number_input("Initial Level Lake 2 (m)", -1e9, 1e9, 200, 1)
-    H_thresh = st.number_input("Initial Threshold Level (m)", -1e9, 1e9, 50, 9, 10)
-    distance_between_lakes = st.number_input("Lake Distance (m)", .1, 1e9, 500, 100)
+    H1_init = st.text_input("Initial Level Lake 1 (m)", -1e9, 1e9, 500, 10)
+    H2_init = st.text_input("Initial Level Lake 2 (m)", -1e9, 1e9, 200, 1)
+    H_thresh = st.text_input("Initial Threshold Level (m)", -1e9, 1e9, 50, 9, 10)
+    distance_between_lakes = st.text_input("Lake Distance (m)", .1, 1e9, 500, 100)
 with col2:
-    A1 = st.number_input("Area Lake 1 (m²)", 1, 1e19, 5e6, 1e6)
-    A2 = st.number_input("Area Lake 2 (m²)", 1, 1e19, 5e6, 1e6)
-    width_factor = st.number_input("Width Factor", .5, 100, 1, 1)
+    A1 = st.text_input("Area Lake 1 (m²)", 1, 1e19, 5e6, 1e6)
+    A2 = st.text_input("Area Lake 2 (m²)", 1, 1e19, 5e6, 1e6)
+    width_factor = st.text_input("Width Factor", .5, 100, 1, 1)
 with col3:
-    total_time = st.number_input("Simulation Time (s)", 10000, 100000, 36000, 5000)
-    erosion_factor = st.number_input("Erosion Rate", 0.001, 0.1, 0.01, 0.001)
-    mannings_n = st.number_input("Manning's n", 0.01, 0.1, 0.03, 0.005)
+    total_time = st.text_input("Simulation Time (s)", 10000, 100000, 36000, 5000)
+    erosion_factor = st.text_input("Erosion Rate", 0.001, 0.1, 0.01, 0.001)
+    mannings_n = st.text_input("Manning's n", 0.01, 0.1, 0.03, 0.005)
  
 # Constants
 g = 9.81  # Gravity acceleration (m/s²)
