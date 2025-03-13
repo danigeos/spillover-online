5b398afe0b168c5db214fc3a9c14d63b5d6064dc
Δρ(Garcia-Castellanos) <d.g.c@csic.es>
2025-03-13 13:49:56 +0100

    commit

--- spillover+grainsize.py
+++ spillover+grainsize.py
@@ -112,6 +112,7 @@ ax3 = ax1.twinx()
 
 ax3.spines["right"].set_position(("outward", 60))  # Offset third axis for clarity
 
+erosion_rates /= 1e3
 ax1.plot(time_steps / 3600, Q_values, color='blue')
 ax2.plot(time_steps / 3600, erosion_rates, color='green')
 ax3.plot(time_steps / 3600, velocity_values, color='purple')
